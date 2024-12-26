3a939eb87eab9e4e692785252ea33186a3d4056f
Anmar Abbas DataGo <anmar@8020datago.ai>
2024-12-26 10:26:34 +0000

    y

---
+++ models/1_stg/sql_erp/stg_petshop_inbound_sales_header.sql
@@ -0,0 +1,12 @@
+With source as (
+ select * from {{ source(var('erp_source'), 'petshop_inbound_sales_header_c91094c2_db03_49d2_8cb9_95c179ecbf9d') }}
+)
+select 
+*,
+
+current_timestamp() as ingestion_timestamp,
+
+
+
+
+from source 
---
+++ models/1_stg/sql_erp/stg_petshop_value_entry.sql
@@ -0,0 +1,12 @@
+With source as (
+ select * from {{ source(var('erp_source'), 'petshop_value_entry_437dbf0e_84ff_417a_965d_ed2bb9650972') }}
+)
+select 
+*,
+
+current_timestamp() as ingestion_timestamp,
+
+
+
+
+from source 
